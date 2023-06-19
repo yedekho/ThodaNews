1c7376eb68fcf167dc0b51a54a3681c3504bf573
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-06-19 04:06:50 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -453,7 +453,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('❓ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', callback_data='whyjoin')
+            InlineKeyboardButton('❓ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url ='https://github.com/crazyDeveloperz/Joker-Robot')
         ], [
             InlineKeyboardButton('⇍ ʙᴀᴄᴋ ⇏', callback_data='about')
         ]]
