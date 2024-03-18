9cbea725b83908d3ad260c39b24442937813656e
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-03-18 11:18:47 -0700

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -141,13 +141,11 @@ async def re_enable_chat(bot, message):
 async def get_ststs(bot, message):
     rju = await message.reply('Fetching stats..')
     total_users = await db.total_users_count()
-    totl_chats = await db.total_chat_count()
-    files = await Media.count_documents()
     size = await db.get_db_size()
     free = 536870912 - size
     size = get_size(size)
     free = get_size(free)
-    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
+    await rju.edit(script.STATUS_TXT.format(total_users, size, free))
 
 
 # a function for trespassing into others groups, Inspired by a Vazha
