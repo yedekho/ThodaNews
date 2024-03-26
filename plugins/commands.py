684296b19c9f08004f59d37eb705b45f74bc197c
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-03-25 22:51:02 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,8 +54,9 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('Telegram', url=f"http://telegram.me/wownewsss")
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
