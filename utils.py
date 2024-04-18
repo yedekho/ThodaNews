e4b5fc6199ee95bff25e217220e77b3d95ab8215
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2024-04-18 22:01:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -153,7 +153,7 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
     except Exception as e:
         return False, "Error"
-
+        
 async def search_gagala(text):
     usr_agent = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
